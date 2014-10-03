0073c6ef67bbe48b91b8b0418d917a0be52d5175
dabing1022 <dabing1022@gmail.com>
2014-10-03 10:33:17 +0800

    add RogueDRT add-on
    add RogueDRT add-on

---
+++ RogueDRT/RogueDRT.lua
@@ -0,0 +1,10 @@
+﻿USD="UNIT_SPELLCAST_SUCCEEDED";OT="OnEvent";FR="Frame";RF=CreateFrame;RD="Border";UE=UnitName
+CS=RF(FR) CS.c=RF("Cooldown","CST",CS.t) CS:RegisterEvent(USD)
+CS.c:SetAllPoints(CS) CS:SetPoint("CENTER",UIParent,"CENTER",-80,0)CS:SetSize(20,20)CS.t=CS:CreateTexture(nil,RD)CS.t:SetAllPoints()CS.t:SetTexture("Interface\\Icons\\ability_cheapshot")
+CS:SetScript(OT,function(self,event,...) if UE(select(1,...))==UE("player") and select(5,...)==1833 then CST:SetCooldown(GetTime(), 23)end if UE(select(1,...))==UE("player") and select(5,...)==408 then CST:SetCooldown(GetTime(), 25) end end)
+SP=RF(FR) SP.c=RF("Cooldown","SAP",SP.t) SP:RegisterEvent(USD)
+SP.c:SetAllPoints(SP) SP:SetPoint("CENTER",UIParent,"CENTER",-80,20)SP:SetSize(20,20)SP.t=SP:CreateTexture(nil,RD)SP.t:SetAllPoints()SP.t:SetTexture("Interface\\Icons\\ability_sap")
+SP:SetScript(OT,function(self,event,...) if UE(select(1,...))==UE("player") and select(5,...)==6770 then SAP:SetCooldown(GetTime(), 27)end if UE(select(1,...))==UE("player") and select(5,...)==1776 then SAP:SetCooldown(GetTime(), 23) end end)
+GR=RF(FR) GR.c=RF("Cooldown","GARROTE",GR.t) GR:RegisterEvent(USD)
+GR.c:SetAllPoints(GR) GR:SetPoint("CENTER",UIParent,"CENTER",-80,40)GR:SetSize(20,20)GR.t=GR:CreateTexture(nil,RD)GR.t:SetAllPoints()GR.t:SetTexture("Interface\\Icons\\ability_Rogue_Garrote")
+GR:SetScript(OT,function(self,event,...) if UE(select(1,...))==UE("player") and select(5,...)==703 then GARROTE:SetCooldown(GetTime(), 23)end if UE(select(1,...))==UE("player") and select(5,...)==102926 then GARROTE:SetCooldown(GetTime(), 20) end end)
