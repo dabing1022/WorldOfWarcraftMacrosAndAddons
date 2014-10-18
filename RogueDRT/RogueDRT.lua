0912f1441b8d43a6b598282ab1137b210ffe8c5b
ChildhoodAndy <dabing1022@gmail.com>
2014-10-18 14:42:17 +0800

    update RogueDRT
    update RogueDRT

--- RogueDRT/RogueDRT.lua
+++ RogueDRT/RogueDRT.lua
@@ -9,5 +9,23 @@ GR=RF(FR) GR.c=RF("Cooldown","GARROTE",GR.t) GR:RegisterEvent(USD)
 GR.c:SetAllPoints(GR) GR:SetPoint("CENTER",UIParent,"CENTER",-260,345)GR:SetSize(30,30)GR.t=GR:CreateTexture(nil,RD)GR.t:SetAllPoints()GR.t:SetTexture("Interface\\Icons\\ability_Rogue_Garrote")
 GR:SetScript(OT,function(self,event,...) if UE(select(1,...))==UE("player") and select(5,...)==703 then GARROTE:SetCooldown(GetTime(), 23)end if UE(select(1,...))==UE("player") and select(5,...)==102926 then GARROTE:SetCooldown(GetTime(), 20) end end)
 
-UFP = "UnitFramePortrait_Update"; UICC = "Interface\\TargetingFrame\\UI-Classes-Circles"; CIT = CLASS_ICON_TCOORDS;SP,CA="SetPoint","ClearAllPoints";NO=function() end
-hooksecurefunc(UFP,function(s) if s.portrait then if UnitIsPlayer(s.unit) then local t=CIT[select(2,UnitClass(s.unit))] if t then s.portrait:SetTexture(UICC) s.portrait:SetTexCoord(unpack(t)) end else s.portrait:SetTexCoord(0,1,0,1) end end end)
+-- UFP = "UnitFramePortrait_Update"; UICC = "Interface\\TargetingFrame\\UI-Classes-Circles"; CIT = CLASS_ICON_TCOORDS;SP,CA="SetPoint","ClearAllPoints";NO=function() end
+-- hooksecurefunc(UFP,function(s) if s.portrait then if UnitIsPlayer(s.unit) then local t=CIT[select(2,UnitClass(s.unit))] if t then s.portrait:SetTexture(UICC) s.portrait:SetTexCoord(unpack(t)) end else s.portrait:SetTexCoord(0,1,0,1) end end end)
+local UFP = "UnitFramePortrait_Update"
+local UICC = "Interface\\TargetingFrame\\UI-Classes-Circles"
+local CIT = CLASS_ICON_TCOORDS
+
+hooksecurefunc(UFP,function(self)
+        if self.portrait then
+                if UnitIsPlayer(self.unit) then				
+					SetPortraitToTexture("PlayerPortrait", "Interface\\AddOns\\MinyonCF\\Coolface")				
+					if self.unit == "player" or self.unit == "pet" or self.unit == "partypet1" or self.unit == "partypet2" or self.unit == "partypet3" or self.unit == "partypet4" then 
+						return 
+					end				
+                    self.portrait:SetTexture(UICC)
+                    self.portrait:SetTexCoord(unpack(CIT[select(2,UnitClass(self.unit))]))
+                else
+                    self.portrait:SetTexCoord(0,1,0,1)
+                end
+        end
+end)
